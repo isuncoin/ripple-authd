7818ed22dfa61c60d0be1ad791f71d247b3a77b1
David Wee <rook2pawn@gmail.com>
2014-08-04 13:59:32 -0700

    initial work on adding ddos / removing configure

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ var express = require('express')
   , http = require('http')
   , https = require('https')
   , path = require('path')
-  , config = require('./config');
+  , config = require('./config')
+  , Ddos= require('ddos');
+
+var ddos = new Ddos;
 
 var app = express();
 
@@ -43,3 +46,43 @@ app.post('/api/sign', api.sign);
 server.listen(app.get('port'), function(){
   console.log("Ripple authd listening on port " + app.get('port'));
 });
+/*
+var express = require('express')
+  , api = require('./routes/api')
+  , fs = require('fs')
+  , path = require('path')
+  , http = require('http')
+  , https = require('https')
+  , path = require('path')
+  , config = require('./config');
+
+var app = express();
+
+var env = process.env.NODE_ENV || 'development';
+
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.options("/api/sign", api.cors);
+app.post('/api/sign', api.sign);
+
+var server;
+if (env === 'development') {
+  app.set('port', config.port || 3000);
+  server = http.createServer(app);
+  app.use(express.errorHandler());
+} else if (env === 'production') {
+  app.set('port', config.port || 443);
+  server = https.createServer({
+    ca: fs.readFileSync(path.resolve(__dirname, 'ca.crt')),
+    key: fs.readFileSync(path.resolve(__dirname, 'ssl.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'ssl.crt'))
+  }, app);
+}
+
+
+server.listen(app.get('port'), function(){
+  console.log("Ripple authd listening on port " + app.get('port'));
+});
+*/
